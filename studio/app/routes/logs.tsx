a4a9fb26b9c1206f0b436b8c431216638e8da3b4
Jeb Feng <ranzeplay@outlook.com>
2025-04-16 10:12:50 +0800

    Implement log view

--- studio/app/routes/logs.tsx
+++ studio/app/routes/logs.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
 import FrameView from "~/components/frame";
 import { TableHeader, TableRow, TableHead, TableBody, TableCell, Table } from "~/components/ui/table";
+import { serverAddress } from "~/lib/env";
+
+type Log = {
+	id: number;
+	level: "info" | "warn" | "error";
+	category: string;
+	message: string;
+	createAt: string;
+};
 
 export default function LogsPage() {
+	const [logs, setLogs] = useState<Log[]>([]);
+	useEffect(() => {
+		fetch(serverAddress + "/api/logs")
+			.then((response) => response.json())
+			.then((data) => setLogs(data))
+			.catch((error) => console.error("Error fetching logs:", error));
+	}, []);
+
 	return (
 		<FrameView title="Logs" subtitle="View logs">
 			<div className="flex flex-col space-y-4">
@@ -15,12 +33,14 @@ export default function LogsPage() {
 						</TableRow>
 					</TableHeader>
 					<TableBody>
-						<TableRow>
-							<TableCell className="font-medium">{new Date().toUTCString()}</TableCell>
-							<TableCell><p className="text-red-500">Error</p></TableCell>
-							<TableCell>Runner</TableCell>
-							<TableCell>Exception occurred while executing a pipeline</TableCell>
-						</TableRow>
+						{logs.map((log) => (
+							<TableRow key={log.id}>
+								<TableCell className="text-xs">{new Date(log.createAt).toLocaleString()}</TableCell>
+								<TableCell className={`text-xs ${log.category == "info" && "text-blue-500"} ${log.category == "warn" && "text-yellow-600"} ${log.category == "error" && "text-red-500"}`}>{log.level}</TableCell>
+								<TableCell className="text-xs">{log.category}</TableCell>
+								<TableCell className="text-xs">{log.message}</TableCell>
+							</TableRow>
+						))}
 					</TableBody>
 				</Table>
 			</div>
