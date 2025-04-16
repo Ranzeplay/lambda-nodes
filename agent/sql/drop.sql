44e983da8f1cb094edae452a88f03006c8c2cce7
Jeb Feng <ranzeplay@outlook.com>
2025-04-16 15:59:16 +0800

    Update drop.sql

--- agent/sql/drop.sql
+++ agent/sql/drop.sql
@@ -4,6 +4,9 @@ BEGIN;
 DROP TABLE IF EXISTS pipelines;
 DROP TABLE IF EXISTS nodes;
 DROP TABLE IF EXISTS logs;
+DROP TABLE IF EXISTS history;
+DROP TABLE IF EXISTS routes;
+DROP TABLE IF EXISTS cron_jobs;
 
 -- Drop custom types
 DROP TYPE IF EXISTS log_level;
